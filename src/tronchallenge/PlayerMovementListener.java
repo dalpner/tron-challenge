9dab18baa413eb75e4e052c6578a492b84abd00d
dalpner <74786569+dalpner@users.noreply.github.com>
2021-10-30 23:36:24 +0200

    Abfrage ob Leben vorhanden

--- src/tronchallenge/PlayerMovementListener.java
+++ src/tronchallenge/PlayerMovementListener.java
@@ -13,27 +13,28 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class PlayerMovementListener implements Listener {
 
 
-    //TODO Was passiert wenn Spieler keine Leben mehr?
-
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         Block block = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
-        if (Colornames.standOnColor(player, block)) {
-            if (block.getType() != TronChallenge.tronChallenge.getPlayerColorHashMap().get(player.getUniqueId())) {
-                player.setHealth(0);
-                TronChallenge.tronChallenge.addPlayerLifeHashMap(player.getUniqueId(), TronChallenge.tronChallenge.getPlayerLifeHashMap().get(player.getUniqueId())-1);
-                if (TronChallenge.tronChallenge.getPlayerLifeHashMap().get(player.getUniqueId()) <=0){
-                    player.setGameMode(GameMode.SPECTATOR);
-                    event.getPlayer().sendMessage("Du hast leider keine Leben mehr. Ab jetzt heißt es zuschauen!");
-                }
-                event.getPlayer().sendMessage("Current lifes: " + TronChallenge.tronChallenge.getPlayerLifeHashMap().get(player.getUniqueId()));
-            }
-        }
 
-        if (block.getType() != Material.OBSIDIAN && block.getType() != Material.AIR && block.getType() != Material.WATER) {
-            World w = player.getWorld();
-            w.getBlockAt(block.getLocation()).setType(TronChallenge.tronChallenge.getSpezColorNum(player.getUniqueId()));
+        if (TronChallenge.tronChallenge.getPlayerLifeHashMap().get(player.getUniqueId()) > 0) {
+
+            if (Colornames.standOnColor(player, block)) {
+                if (block.getType() != TronChallenge.tronChallenge.getPlayerColorHashMap().get(player.getUniqueId())) {
+                    player.setHealth(0);
+                    TronChallenge.tronChallenge.addPlayerLifeHashMap(player.getUniqueId(), TronChallenge.tronChallenge.getPlayerLifeHashMap().get(player.getUniqueId()) - 1);
+                    event.getPlayer().sendMessage("Current lifes: " + TronChallenge.tronChallenge.getPlayerLifeHashMap().get(player.getUniqueId()));
+                }
+            }
+
+            if (block.getType() != Material.OBSIDIAN && block.getType() != Material.AIR && block.getType() != Material.WATER) {
+                World w = player.getWorld();
+                w.getBlockAt(block.getLocation()).setType(TronChallenge.tronChallenge.getSpezColorNum(player.getUniqueId()));
+            }
+        } else {
+            player.setGameMode(GameMode.SPECTATOR);
+            event.getPlayer().sendMessage("Du hast leider keine Leben mehr. Ab jetzt heißt es zuschauen!");
         }
     }
 }
